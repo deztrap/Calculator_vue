233fcfd4d9a098ead2d4f36231baec433bc49b69
Tihitena <tihitenamesfin111@gmail.com>
2023-02-13 17:47:53 +0300

    added calculator elements with some style

---
+++ src/components/CalculatorItems.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+    <div class="p-3" style="max-width: 400px; margin: 50px; background: #234">
+      <div
+        class="
+          w-full
+          rounded
+          m-1
+          p-3
+          text-right
+          lead
+          font-weight-bold
+          text-white
+        "
+        style="background: #31475e"
+      >
+        {{ CalculatorResult || 0 }}
+      </div>
+      <div class="row no-gutters">
+        <div class="col-3" v-for="element in CalculatorItems" :key="element">
+          <div
+            class="
+              lead
+              dark-bg
+              text-center
+              m-1
+              py-3
+              text-white
+              rounded
+              hover-class
+            "
+            :class="{
+              highlight_bg: ['C', '+', '-', '/', 'x', '%', '='].includes(
+                element
+              ),
+            }"
+            @click="calculate(element)"
+          >
+            {{ element }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <p>{{ operators }}</p>
+    <input placeholder="enter number" />
+  </div>
+</template>
+<script>
+export default {
+  name: "CalculatorItem",
+  data: function () {
+    return {
+      CalculatorResult: "",
+      CalculatorItems: [
+        "C",
+        "+",
+        "-",
+        "/",
+        7,
+        8,
+        9,
+        "x",
+        4,
+        5,
+        6,
+        "%",
+        1,
+        2,
+        3,
+        "=",
+        "0",
+        ".",
+      ],
+      operators: ["+", "-", "*", "/", "="],
+    };
+  },
+  methods: {
+    calculate(element) {
+      this.CalculatorResult += element;
+    },
+  },
+};
+</script>
+<style>
+.text-right {
+  text-align: right;
+}
+.hover-class:hover {
+  cursor: pointer;
+  background-color: #1a1720;
+}
+.dark-bg {
+  background: #31475e;
+}
+.highlight_bg {
+  background: rgb(140, 8, 235);
+}
+</style>
